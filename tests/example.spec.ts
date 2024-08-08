bcd623a012913f7d1b123bbd3cfc8cbb6d7e94f6
Shaharyar Aziz <shaharyaraziz462@gmail.com>
2024-08-08 12:08:37 +0500

    new changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,7 +6,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/EMR - Shifa International Hospitals Ltd./);
 });
-
+ 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
