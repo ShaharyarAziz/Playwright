424577b0b3d1c657616af952cd32d48cb4e102a4
ShaharyarAziz <shaharyaraziz462@gmail.com>
2025-07-13 21:53:29 +0500

    ER Project

---
+++ tests/EMR_ER/Test_scripts/login.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../Pages/loginPage";
+import * as dotenv from 'dotenv';
+dotenv.config(); // Load .env variables
+
+test.describe("Login Test with POM", () => {
+  test("User can log in and click Unit", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    // Use env variables
+    const username = process.env.ER_USERNAME!;
+    const password = process.env.ER_PASSWORD!;
+    const patientName = process.env.PATIENT_NAME!;
+
+    await loginPage.goto();
+    await loginPage.login(username, password);
+    await loginPage.clickUnit();
+    await loginPage.ForMRNumberSearch();
+    await loginPage.TypeInMRNumberSearch(patientName); 
+    await loginPage.clickMRNumberSearchButton();
+    await loginPage.selectPatient();
+    await page.pause(); // for debugging 
+  });
+});
